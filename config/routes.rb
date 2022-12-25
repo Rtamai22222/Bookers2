42e90bc8758de291f9810179c06e8e855a795bb3
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-25 13:43:00 +0000

    [Add] BootstrapとFontAwesomeの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
-  # get 'homes/top'
-  # get 'homes/about'
-  # get 'books/new'
-  # get 'users/show'
-  # get 'users/edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
