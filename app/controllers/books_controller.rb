e68e8227102891aa83c6ce777f752eb1c5062fcd
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-29 09:29:49 +0000

    [Add] bookの投稿&保存の機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,27 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    @books = Book.all
+    @book = Book.new
+    # @user_image =
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+     params.require(:book).permit(:title, :body)
   end
 end
